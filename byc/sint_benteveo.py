55610bfd7e2ed69525b0ca9a56aa14b9ee70ec52
mwappner <42619775+mwappner@users.noreply.github.com>
2019-06-07 11:48:22 -0300

    La buena parametrización

--- byc/sint_benteveo.py
+++ byc/sint_benteveo.py
@@ -15,8 +15,9 @@ import os
 
 cant_sintesis = 1 #cuantos cantos voy a sintetizar
 nombre_base = 'benteveo' #nombre de los sonogramas
-path_sono = os.path.join('limpiando', 'sonogramas')
-path_audio = os.path.join('limpiando', 'audios')
+path_sono = os.path.join('nuevos', 'sonogramas')
+path_audio = os.path.join('nuevos', 'audios')
+#path_sono = path_audio = 'filtro'
 
 import numpy as np
 from numpy.random import normal
@@ -36,10 +37,10 @@ global estimulo1
 global destimulodt1
 
 gamma = 24000
-uoch, uolb, uolg, rb, rdis = (350/2.0)*100000000, 0.1 , 1/35., 10000000, 10000  # 24*10000 , y con 350/3.0, la frec de la oec en 4000 Hz
-fsamp, t0, tf, L = 882000.0, 0, 0.5, 0.045
+uoch, uolb, uolg, rb, rdis = (250/2.0)*1e8, 0.1 , 1/35., 1e7, 1e4   # 24*10000 , y con 350/3.0, la frec de la oec en 4000 Hz
+fsamp, L = 882000.0, 0.045
 dt = 1/fsamp
-tiempo_total = 1.2
+tiempo_total = 0.86
 N=int((L/(350*dt))//1)
 
 cant_puntos = np.int(tiempo_total/(dt))
@@ -68,38 +69,113 @@ def rk4(dv, v, dt, kappa, b):
     v += dt6 * ( 2*(k2+k3) + k1 + k4)
     return v
 
-
-def expo(ti, tf, wi, wf, f, freqs, beta, amps):
+def forma_amps(inicio, fin):
+    if inicio and fin:
+        return lambda t: np.sin(np.pi * t)
+    if inicio and not fin:
+        return lambda t: np.sin(np.pi * t / 2)
+    if not inicio and fin:
+        return lambda t: np.sin((np.pi * (t+1) / 2))
+    else: 
+        return lambda t: 1                               
+            
+def expo(ti, tf, wi, wf, f, freqs, beta, amps, tau=3,
+         param=1, d=0, inicio=True, fin=True):
     
     i=np.int(ti/dt)
     j=np.int(tf/dt)
-    k = np.arange(j-i) / (j-i) # =  t - ti / (ti - tf)
+    dj = j-i
+    k = np.arange(dj) / dj # =  t - ti / (tf - ti)
+    amps[i:j] = f * forma_amps(inicio, fin)(k)
+    
+    if param == 2:
+        l = int(d/dt)
+        i -= l  
+        k = np.arange(-l, dj) / dj
 
-    freqs[i:j] = wf + (wi-wf) * np.exp(-3 * k)
+    freqs[i:j] = wf + (wi-wf) * np.exp(-tau * k)
     beta[i:j] = .5
-    amps[i:j] = f * np.sin(np.pi * k)
 
-def rectas(ti, tf, wi, wf, f, freqs, beta, amps):
+def rectas(ti, tf, wi, wf, f, freqs, beta, amps, 
+            param=1, d=0, inicio=True, fin=True):
     
+    if param not in (1,2):
+        raise ValueError("parametrizacion debe ser 1 o 2")
+        
     i=np.int(ti/dt)
     j=np.int(tf/dt)
-    k = np.arange(j-i) / (j-i) # =  t - ti / (ti - tf)
+    dj = j-i
+    k = np.arange(dj) / dj # =  t - ti / (ti - tf)
+    amps[i:j] = f * forma_amps(inicio, fin)(k)
 
+    if param == 2:
+        l = int(d/dt)
+        i -= l  
+        k = np.arange(-l, dj) / dj
+        
     freqs[i:j] = wi + (wf-wi) * k
     beta[i:j] = .5
-    amps[i:j] = f * np.sin(np.pi * k)
 
-def senito(ti, tf, media, amplitud, alphai, alphaf, f, freqs, beta, amps):
+def senito(ti, tf, media, amplitud, alphai, alphaf,
+           f, freqs, beta, amps, param=1, d=0, inicio=True, fin=True):
+    '''Param=1 corresponde a la aprametrización usual con (ti, tf, media, amplitud
+    alphai, alphaf), mientras que param=2 corresponde a una parametrización que 
+    deriva de lo anterior y que comienza en t<ti, por lo que pide d para armar un
+    ti_nuevo = ti-d.'''
     
+    if param not in (1,2):
+        raise ValueError("parametrizacion debe ser 1 o 2")
+
     i=np.int(ti/dt)
     j=np.int(tf/dt)
-    k = np.arange(j-i)
+    dj = j-i
+    k = np.arange(0,1,1/dj)
+    amps[i:j]= f * forma_amps(inicio, fin)(k)
     
-    freqs[i:j] = media + amplitud * np.sin(alphai + (alphaf - alphai) * k / (j-i))
-    beta[i:j] = .5
-    new_k = 5* k / (j-i) # = k/tau
-    amps[i:j] = f * new_k * np.exp(-new_k) * normal(1, .1) * (1 + .4 * np.sin(2*np.pi * k / 6820))
+    if param==1:
+        freqs[i:j] = media + amplitud * np.sin(alphai + (alphaf - alphai) * k)
+        beta[i:j] = .5
+    else:
+        l = int(d/dt)
+        i2= i-l   
+        k = np.arange(i2, j)
+        
+        phi = (alphai * j - alphaf * i) / dj
+        omega = (alphaf - alphai) / dj
+        freqs[i2:j] = media + amplitud * np.sin(phi + omega * k )
+        beta[i2:j] = .5
 
+def senpol(ti, tf, media, amplitud, alphai, alphaf, grado,
+           f, freqs, beta, amps, param=1, d=0, inicio=True, fin=True):
+    '''Param=1 corresponde a la aprametrización usual con (ti, tf, media, amplitud
+    alphai, alphaf), mientras que param=2 corresponde a una parametrización que 
+    deriva de lo anterior y que comienza en t<ti, por lo que pide d para armar un
+    ti_nuevo = ti-d.'''
+    
+    if param not in (1,2):
+        raise ValueError("parametrizacion debe ser 1 o 2")
+
+    i=np.int(ti/dt)
+    j=np.int(tf/dt)
+    dj = j-i
+    k = np.arange(0,1,1/dj)
+    amps[i:j]= f * forma_amps(inicio, fin)(k)
+    
+    if param==1:
+        freqs[i:j] = media + amplitud * np.sin(alphai + (alphaf - alphai) * k)
+        beta[i:j] = .5
+    else:
+        l = int(d/dt)
+        i2= i-l   
+        k = np.arange(i2, j)
+        
+        phi = (alphai * j - alphaf * i) / dj
+        omega = (alphaf - alphai) / dj
+        freqs[i2:j] = media + amplitud * (np.sin(phi + omega * k ) ** grado)
+        beta[i2:j] = .5
+
+
+#%%
 for i in range(cant_sintesis):
     
     #reinicio valores
@@ -109,13 +185,60 @@ for i in range(cant_sintesis):
     
     v = np.array([0.01, 0.001, 0.001, 0.0001, 0.0001])
 
-# -----------------------------------
-# Genero los parámetros de los cantos
-# -----------------------------------    
-    senito(0.166,0.32+0.05*normal(1,0.1),1310*0.5*normal(1,0.1),200*0.5*normal(1,0.1),0,np.pi,0.7*1.1,frecuencias,beta,amplitudes)
-    senito(0.58,0.7,1305*0.5*normal(1,0.1),600*0.5*normal(1,0.1),-np.pi/4.0,3*np.pi/2.0,0.7*1,frecuencias,beta,amplitudes)
-    senito(0.74+0.05*normal(1,0.1),1.06,1301,200*normal(1,0.05),0,np.pi+np.pi/4.0,0.7*1,frecuencias,beta,amplitudes)
+    # -----------------------------------
+    # Genero los parámetros de los cantos
+    # ----------------------------------- 
+
+    ### benteveo_BVRoRo_highpass_notch
+    f = 1
+    senito(ti=0.098, tf=0.22, media=-70, amplitud=1800, alphai=2.44, alphaf=0.7,
+          f=f*1.6, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.05, 
+          fin=False)
+    rectas(ti=0.22, tf=0.23, wi=1100, wf=790,
+          f=f*1.3, freqs=frecuencias, beta=beta, amps=amplitudes, 
+          inicio=False)
     
+#    #A: opcion 1
+#    senito(ti=0.384, tf=0.422, media=-900, amplitud=2530, alphai=2.35, alphaf=1.29,
+#          f=f, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.05, 
+#          fin=False)
+#    expo(ti=0.422, tf=0.476, wi=1530, wf=700, tau=2.1,
+#        f=f, freqs=frecuencias, beta=beta, amps=amplitudes, 
+#        inicio=False)
+    #A: opcion 2 (mejor)
+    medio1, medio2 = 0.422, 0.45
+    senito(ti=0.384, tf=medio1, media=-900, amplitud=2530, alphai=2.35, alphaf=1.29,
+          f=f*1.5, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.05, 
+          fin=False)
+    expo(ti=medio1, tf=medio2, wi=1530, wf=1100, tau=3.4,
+        f=f, freqs=frecuencias, beta=beta, amps=amplitudes, 
+        inicio=False, fin=False)
+    senito(ti=medio2, tf=0.476, media=-1260, amplitud=2380, alphai=7.8, alphaf=7.3,
+          f=f, freqs=frecuencias, beta=beta, amps=amplitudes, 
+          inicio=False)
+    
+#    #B: opcion 1
+#    senito(ti=0.582, tf=0.831, media=-410, amplitud=1900, alphai=2.44, alphaf=0.7,
+#          f=f, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.05)
+#    #B: opcion 2 (mejor)
+#    senito(ti=0.582-0.06, tf=0.831+0.02, media=-7300, amplitud=8700, alphai=1.86, alphaf=1.22,
+#          f=f, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.03)         
+    #B: opcion 3 (mejor)
+    senpol(ti=0.582-0.06, tf=0.831+0.02, media=-7300, amplitud=8700, alphai=1.86, alphaf=1.22,
+          grado=1, f=f, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.03)      
+    
+    corrimiento = 200
+    donde = frecuencias != 0
+    frecuencias[donde] = frecuencias[donde] - corrimiento
+
+#    #ploteo los parámetros
+#    tiempo = np.linspace(0, tiempo_total, cant_puntos)
+#    fig1, axs= plt.subplots(3,1, sharex=True)
+#    axs[0].plot(tiempo[::10],frecuencias[::10], '.')
+#    axs[1].plot(tiempo[::10],amplitudes[::10], '.')
+#    axs[2].plot(tiempo[::10],beta[::10], '.')
+
+#%%
 # -------
 # Integro
 # -------
@@ -126,7 +249,7 @@ for i in range(cant_sintesis):
     
     print('integrando...')
     
-    kappa_todos = (6.56867694e-08 * frecuencias*frecuencias + 4.23116382e-05 * frecuencias + 2.67280260e-02) * normal(1,0.2,cant_puntos)
+    kappa_todos = (6.56867694e-08 * frecuencias*frecuencias + 4.23116382e-05 * frecuencias + 2.67280260e-02)
     b_todos = beta * normal(1, .1, cant_puntos)
     
     for kappa, b in zip(kappa_todos, b_todos):
@@ -147,17 +270,20 @@ for i in range(cant_sintesis):
     
     
     sonido = np.array(v4) * amplitudes
-    sonido *= 1000
-    sonido += 20 * normal(0, .007, len(sonido))
+#    sonido *= 1000
+    sonido += 10 * normal(0, 7e-5, len(sonido))
     
     f, t, Sxx = signal.spectrogram(sonido, fsamp, window=('gaussian',20*128),
                                    nperseg=10*1024, noverlap=18*512, scaling='spectrum')
     
     fig, ax = plt.subplots()
-    ax.pcolormesh(t,f,np.log10(Sxx),rasterized=True,cmap=plt.get_cmap('Greys'))
-    ax.set_ylim(10,18000)
+    ax.pcolormesh(t,f,np.log10(Sxx),rasterized=True,
+                  cmap=plt.get_cmap('Greys'))
+    ax.set_ylim(10,8000)
     ax.axis('off')
-    fig.subplots_adjust(bottom = 0, top = 1, left = 0, right = 1) #para que no tenga bordes blancos
+#    ax.grid()
+    fig.subplots_adjust(bottom = 0, top = 1, left = .08, right = 1) #para que no tenga bordes blancos
+    
     
     nombre = creo_nombre(path_sono, nombre_base, '.jpg')
     fig.savefig(nombre, dpi=100)
@@ -165,7 +291,7 @@ for i in range(cant_sintesis):
     
     scaled = (sonido/np.max(np.abs(sonido))).astype(np.float32)
     nombre = creo_nombre(path_audio, nombre_base, '.wav')
-    write(nombre, int(fsamp), scaled)
+    write(nombre, int(fsamp/20), scaled[::20])
     
     
     print('listo {} de {}!'.format(i+1, cant_sintesis))
--- byc/sint_chingolo.py
+++ byc/sint_chingolo.py
@@ -12,10 +12,10 @@ it creates wav
 """
 import os
 
-cant_sintesis = 2 #cuantos cantos voy a sintetizar
+cant_sintesis = 1 #cuantos cantos voy a sintetizar
 nombre_base = 'chingolo' #nombre de los sonogramas
-path_sono = os.path.join('limpiando', 'sonogramas')
-path_audio = os.path.join('limpiando', 'audios')
+path_sono = os.path.join('nuevos', 'sonogramas')
+path_audio = os.path.join('nuevos', 'audios')
 
 import numpy as np
 from numpy.random import normal
@@ -35,10 +35,10 @@ global estimulo1
 global destimulodt1
 
 gamma=24000
-uoch, uolb, uolg, rb, rdis = (350/5.0)*100000000, 0.0001 , 1/20., 0.5*10000000, 24*10000 # 24*10000 , y con 350/3.0, la frec de la oec en 4000 Hz
-fsamp, t0, tf, L=  882000.0, 0, 0.5, 0.025
+uoch, uolb, uolg, rb, rdis = (350/5.0)*1e8, 1e-4, 1/20., 5e6, 2.4e5 # 2.4e5 , y con 350/3.0, la frec de la oec en 4000 Hz
+fsamp, L=  882000.0, 0.025
 dt = 1/fsamp
-tiempo_total=2.1
+tiempo_total=1.66
 N=int((L/(350*dt))//1)
 
 cant_puntos = np.int(tiempo_total/(dt))
@@ -68,36 +68,85 @@ def rk4(dv, v, dt, kappa, b):
     return v
 
 
-def expo(ti, tf, wi, wf, f, freqs, beta, amps):
+def forma_amps(inicio, fin):
+    if inicio and fin:
+        return lambda t: np.sin(np.pi * t)
+    if inicio and not fin:
+        return lambda t: np.sin(np.pi * t / 2)
+    if not inicio and fin:
+        return lambda t: np.sin((np.pi * (t+1) / 2))
+    else: 
+        return lambda t: 1                    
+            
+def expo(ti, tf, wi, wf, f, freqs, beta, amps, tau=3,
+         param=1, d=0, inicio=True, fin=True):
     
     i=np.int(ti/dt)
     j=np.int(tf/dt)
-    k = np.arange(j-i) / (j-i) # =  t - ti / (ti - tf)
-
-    freqs[i:j] = wf + (wi-wf) * np.exp(-3 * k)
-    beta[i:j] = 1
-    amps[i:j] = f * np.sin(np.pi * k)
-
-def rectas(ti, tf, wi, wf, f, freqs, beta, amps):
+    dj = j-i
+    k = np.arange(dj) / dj # =  t - ti / (tf - ti)
+    amps[i:j] = f * forma_amps(inicio, fin)(k)
     
+    if param == 2:
+        l = int(d/dt)
+        i -= l  
+        k = np.arange(-l, dj) / dj
+
+    freqs[i:j] = wf + (wi-wf) * np.exp(-tau * k)
+    beta[i:j] = .5
+
+def rectas(ti, tf, wi, wf, f, freqs, beta, amps, 
+            param=1, d=0, inicio=True, fin=True):
+    
+    if param not in (1,2):
+        raise ValueError("parametrizacion debe ser 1 o 2")
+        
     i=np.int(ti/dt)
     j=np.int(tf/dt)
-    k = np.arange(j-i) / (j-i) # =  t - ti / (ti - tf)
+    dj = j-i
+    k = np.arange(dj) / dj # =  t - ti / (ti - tf)
+    amps[i:j] = f * forma_amps(inicio, fin)(k)
 
+    if param == 2:
+        l = int(d/dt)
+        i -= l  
+        k = np.arange(-l, dj) / dj
+        
     freqs[i:j] = wi + (wf-wi) * k
     beta[i:j] = .5
-    amps[i:j] = f * np.sin(np.pi * k)
 
-def senito(ti, tf, media, amplitud, alphai, alphaf, f, freqs, beta, amps):
+def senito(ti, tf, media, amplitud, alphai, alphaf,
+           f, freqs, beta, amps, param=1, d=0, inicio=True, fin=True):
+    '''Param=1 corresponde a la aprametrización usual con (ti, tf, media, amplitud
+    alphai, alphaf), mientras que param=2 corresponde a una parametrización que 
+    deriva de lo anterior y que comienza en t<ti, por lo que pide d para armar un
+    ti_nuevo = ti-d.'''
     
+    if param not in (1,2):
+        raise ValueError("parametrizacion debe ser 1 o 2")
+
     i=np.int(ti/dt)
     j=np.int(tf/dt)
-    k = np.arange(j-i) / (j-i) # =  t - ti / (ti - tf)
+    dj = j-i
+    k = np.arange(0,1,1/dj)
+    amps[i:j]= f * forma_amps(inicio, fin)(k)
     
-    freqs[i:j] = media + amplitud * np.sin(alphai + (alphaf - alphai) * k)
-    beta[i:j] = .5
-    amps[i:j]= f * np.sin(np.pi * k)
+    if param==1:
+        freqs[i:j] = media + amplitud * np.sin(alphai + (alphaf - alphai) * k)
+        beta[i:j] = .5
+    else:
+        phi = (alphai * j - alphaf * i) / dj
+        omega = (alphaf - alphai) / dj
+        
+        l = int(d/dt)
+        i -= l   
+        k = np.arange(i, j)
+        
+        freqs[i:j] = media + amplitud * np.sin(phi + omega * k )
+        beta[i:j] = .5
 
+
+#%%
 #reinicio valores
 for _ in range(cant_sintesis):
     
@@ -110,22 +159,37 @@ for _ in range(cant_sintesis):
     # -----------------------------------
     # Genero los parámetros de los cantos
     # -----------------------------------
-    rectas(0.03,0.42+0.01*normal(0,1),4529+500*normal(0,1),3200+500*normal(0,1),0.15,frecuencias,beta,amplitudes)
-    rectas(0.535,0.774+0.01*normal(0,1),3990+500*normal(0,1),4300+500*normal(0,1),1,frecuencias,beta,amplitudes)
-    tiempito=1.05+0.01*normal(0,1)
-    senito(0.863,tiempito,6500+500*normal(0,1),900,-np.pi/2.0-np.pi/4.,1*np.pi,1,frecuencias,beta,amplitudes)
-    expo(tiempito,1.288,6500+500*normal(0,1),3460+100*normal(0,1),1.0,frecuencias,beta,amplitudes)
-    fmax=5900+100*normal(0,1)
-    senito(1.33,1.41+0.0005*normal(0,1),fmax,2740,np.pi,3*np.pi/2.0+np.pi/16.0,1,frecuencias,beta,amplitudes)
-    senito(1.42+0.0005*normal(0,1),1.48,fmax,2740,np.pi,3*np.pi/2.0+np.pi/16.0,1,frecuencias,beta,amplitudes)
-    tiempin=0.0005*normal(0,1)
-    senito(1.49+tiempin,1.55+tiempin,fmax,2740,np.pi,3*np.pi/2.0+np.pi/16.0,1,frecuencias,beta,amplitudes)
-    senito(1.57+tiempin,1.62+tiempin,fmax,2740,np.pi,3*np.pi/2.0+np.pi/16.0,1,frecuencias,beta,amplitudes)
-    senito(1.64+tiempin,1.70+tiempin,fmax,2740,np.pi,3*np.pi/2.0+np.pi/16.0,1,frecuencias,beta,amplitudes)
-    senito(1.72+tiempin,1.77+tiempin,fmax,2740,np.pi,3*np.pi/2.0+np.pi/16.0,.9,frecuencias,beta,amplitudes)
-    senito(1.79+tiempin,1.84+tiempin,fmax,2740,np.pi,3*np.pi/2.0+np.pi/16.0,.7,frecuencias,beta,amplitudes)
-    senito(1.87+tiempin,1.92+tiempin,fmax,2740,np.pi,3*np.pi/2.0,.6,frecuencias,beta,amplitudes)
     
+    ### Chingolo_XC462515_denoised
+    f = .5
+    rectas(ti=0.086, tf=0.168, wi=4560-300, wf=4711-300, 
+           f=f, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.03)
+    
+    expo(ti=0.315, tf=0.569, wi=4260, wf=4030, tau=-1.5,
+           f=f, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.03)
+    
+    medio=0.729
+    rectas(ti=0.677, tf=medio, wi=6030, wf=5730,
+         f=f, freqs=frecuencias, beta=beta, amps=amplitudes, param=2, d=0.03,
+         fin=False)
+    expo(ti=medio, tf=0.961, wi=5736, wf=1370, tau=0.8,
+           f=f, freqs=frecuencias, beta=beta, amps=amplitudes, inicio=False)
+    
+    deltat, t0, t1 = 0.0028, 1.08, 1.124
+    paso = deltat + t1 - t0
+    for k in range(7):
+    #        rectas(t0 + paso*k, t1 + paso*k, 6945, 3839,
+    #               f=1, freqs=frecuencias, beta=beta, amps=amplitudes)
+        rectas(t0 + paso*k, t1 + paso*k, 7030, 3760, 
+             f=1, freqs=frecuencias, beta=beta, amps=amplitudes)
+
+    
+    tiempo = np.linspace(0, tiempo_total, cant_puntos)
+    fig1, axs= plt.subplots(3,1, sharex=True)
+    axs[0].plot(tiempo[::10],frecuencias[::10], '.')
+    axs[1].plot(tiempo[::10],amplitudes[::10], '.')
+    axs[2].plot(tiempo[::10],beta[::10], '.')
+#%%
     # -------
     # Integro
     # -------
@@ -157,12 +221,14 @@ for _ in range(cant_sintesis):
         
     sonido = (np.array(v4) * amplitudes * normal(1,0.01))
     sonido *= 1000
-    sonido += 20*normal(0, .007, cant_puntos)
+    sonido += 5*normal(0, .007, cant_puntos)
     
-    f, t, Sxx = signal.spectrogram(sonido,fsamp,window=('gaussian',20*128),nperseg=10*1024,noverlap=18*512,scaling='spectrum')
+    f, t, Sxx = signal.spectrogram(sonido,fsamp,window=('gaussian',20*128),
+                                   nperseg=10*1024,noverlap=18*512,scaling='spectrum')
     fig, ax = plt.subplots()
-    ax.pcolormesh(t,f,np.log10(Sxx),rasterized=True,cmap=plt.get_cmap('Greys'))
-    ax.set_ylim(10,18000)
+    ax.pcolormesh(t,f,np.log10(Sxx),rasterized=True,
+                  cmap=plt.get_cmap('Greys'))
+    ax.set_ylim(10,8000)
     ax.axis('off')
     fig.subplots_adjust(bottom = 0, top = 1, left = 0, right = 1) #para que no tenga bordes blancos
     
@@ -170,9 +236,9 @@ for _ in range(cant_sintesis):
     fig.savefig(nombre, dpi=100)
     plt.close()
     
-    scaled = np.int16(sonido/np.max(np.abs(sonido)) * 32767)
+    scaled = (sonido/np.max(np.abs(sonido))).astype(np.float32)
     nombre = creo_nombre(path_audio, nombre_base, '.wav')
-    write(nombre, int(fsamp), scaled)
+    write(nombre, int(fsamp/20), scaled[::20])
     
     print('listo!')
     print('\a') #sonido al final de la integración
