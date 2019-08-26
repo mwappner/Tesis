eb656b338c0f43218d7d8f4510dd4e964114f72e
mwappner <42619775+mwappner@users.noreply.github.com>
2019-08-26 18:17:14 -0300

    Uso los gráficos con dataframes

--- byc/graficos.py
+++ byc/graficos.py
@@ -37,30 +37,11 @@ def get_resultados(ubicacion):
     try:
         r = contenidos(ubicacion, filter_ext='.txt')[0]
     except IndexError:
-        print(ubicacion, ' no contenìa resultados')
+        print(ubicacion, ' no contenía resultados')
         return
     return extraigo(r)
-    
-#%% Extraigo valores
-todos = {}
-for k in contenidos(ubi):
-    res = get_resultados(k)
-    if res is not None:
-        res['name'] = k
-        todos[os.path.basename(k).replace('byc_pad_', '')] = res
         
-#%% Grafico accuracy
-
-plt.plot(list(todos.keys()), [v['acc'] for v in todos.values()], 'o')
-plt.xticks(rotation='vertical')
-
-# Pad margins so that markers don't get clipped by the axes
-plt.margins(0.2)
-# Tweak spacing to prevent clipping of tick-labels
-plt.subplots_adjust(bottom=0.2)
-plt.grid()
-
-#%% Todos 2 (para DataFrames)
+#%% Extraigo valores
 
 todos_df = []
 
@@ -72,4 +53,36 @@ for k in contenidos(ubi):
         todos_df.append(res)
 
 data = pd.DataFrame(todos_df)
-data.set_index('name', inplace=True)
+data.set_index('nombre', inplace=True)
+
+
+#%% Grafico accuracy
+fig, (acc, prec, rec) = plt.subplots(3,1, sharex=True)
+
+acc.plot(data.index, data['acc'], 'o', color='C2')
+acc.grid()
+acc.set_ylabel('Accuracy')
+
+prec.plot(data.index, data['precB'],'d', label='benteveos')
+prec.plot(data.index, data['precC'],'*', label='chingolos')
+prec.grid()
+prec.legend()
+prec.set_ylabel('Precicion')
+
+rec.plot(data.index, data['recB'],'d', label='benteveos')
+rec.plot(data.index, data['recC'],'*', label='chingolos')
+rec.grid()
+rec.legend()
+rec.set_ylabel('Recall')
+
+rec.set_xlim([-0.2,19.1])
+plt.xticks(rotation=90)
+
+fig.tight_layout()
+fig.subplots_adjust(hspace=0)
+
+
+# Pad margins so that markers don't get clipped by the axes
+plt.margins(0.2)
+# Tweak spacing to prevent clipping of tick-labels
+plt.subplots_adjust(bottom=0.2)
